cd81b166813b3307629b23d9076afcd24102b05c
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2022-05-03 22:49:56 +0200

    Only run correctness tests once

--- code/utils.c
+++ code/utils.c
@@ -87,6 +87,10 @@ void intro(int argc, char **argv, run_variables_t *run_variables){
         git_dirty = "modified";
     }
 
+    // Correctness tests
+    bool shapley_correct = exact_shapley_correct();
+    bool knn_correct = exact_knn_correct();
+
     // Write collected info to the runfile:
     char tmpbuf[512]; 
     add_run_info(run_variables->runfile, "git_status", git_dirty);
@@ -104,7 +108,8 @@ void intro(int argc, char **argv, run_variables_t *run_variables){
     add_run_info(run_variables->runfile, "cpu", tmpbuf);
     add_run_info_raw(run_variables->runfile, "turbo_boost_disabled", intel_turbo_boost_disabled() ? "true" : "false");
     add_run_info(run_variables->runfile, "arguments", args);
-    add_run_info_raw(run_variables->runfile, "shapley_correct", exact_shapley_correct() ? "true" : "false");
+    add_run_info_raw(run_variables->runfile, "shapley_correct", shapley_correct ? "true" : "false");
+    add_run_info_raw(run_variables->runfile, "knn_correct", exact_knn_correct ? "true" : "false");
     add_run_info_int(run_variables->runfile, "num_runs", run_variables->number_of_runs);
     add_run_info_int(run_variables->runfile, "num_input_sizes", run_variables->number_of_input_sizes);
 
@@ -154,6 +159,7 @@ void intro(int argc, char **argv, run_variables_t *run_variables){
            "Shapley correct:         %s\n\n"\
 
            "\033[1mBenchmark information:\033[0m\n"\
+           "Measured algorithm       %s\n"\
            "Number of input sizes:   %d\n"\
            "Input sizes:             %s\n"\
            "Number of runs:          %d\n"
@@ -167,8 +173,9 @@ void intro(int argc, char **argv, run_variables_t *run_variables){
            (intel_turbo_boost_disabled() ? color("Disabled", GREEN) : color("Enabled", RED)),
            run_variables->runfile_path,
            args,
-           (exact_knn_correct() ? color("Correct", GREEN) : color("Incorrect", RED)),
-           (exact_shapley_correct() ? color("Correct", GREEN) : color("Incorrect", RED)),
+           (knn_correct ? color("Correct", GREEN) : color("Incorrect", RED)),
+           (shapley_correct ? color("Correct", GREEN) : color("Incorrect", RED)),
+           "Exact base implementation",
            run_variables->number_of_input_sizes,
            tmpbuf,
            run_variables->number_of_runs
