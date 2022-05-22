f0698a306b03cd4fbcd8c7cc22c488ef4986072b
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2022-05-22 19:33:12 +0200

    Prepare for shapley ops: (Measure only shapley for now)

--- code/base_exact_shapley.c
+++ code/base_exact_shapley.c
@@ -12,7 +12,10 @@
 #include "knn_exact.h"
 #include "combined_exact_knn_shapley.h"
 
+
+// Base implementation
 void compute_single_unweighted_knn_class_shapley(void *context_ptr){
+    // compute_single_unweighted_knn_class_shapley    or     single_unweighted_knn_class_shapley_opt
     context_t *context = (context_t*)context_ptr;
     debug_print("%s", "\nStart Shapley computation:\n");
     for(int j=0; j<context->size_x_tst;j++){
@@ -41,6 +44,16 @@ void compute_single_unweighted_knn_class_shapley(void *context_ptr){
 
     debug_print("%s", "Exact: Shapley Values done :)\n");
 }
+
+
+
+
+
+
+
+
+
+
 
 uint64_t run_shapley(void *context) {  
     context_t *ctx = (context_t *)context;
@@ -90,8 +103,9 @@ uint64_t run_shapley(void *context) {
     uint64_t start_timer, end_timer;
 
 
+    get_true_exact_KNN(context);
     start_timer = start_tsc();
-    combined_knn_shapley_opt(context);
+    single_unweighted_knn_class_shapley_opt(context);
     end_timer = stop_tsc(start_timer);
 
 
@@ -124,7 +138,9 @@ uint64_t run_shapley(void *context) {
     return end_timer;
 }
 
-void current_opt_compute_single_unweighted_knn_class_shapley(void *context_ptr){
+
+// opt1: avoid pointer chasing, use local vars
+void single_unweighted_knn_class_shapley_opt(void *context_ptr){
     context_t *context = (context_t*)context_ptr;
     debug_print("%s", "\nStart Shapley computation:\n");
     
@@ -179,40 +195,4 @@ void current_opt_compute_single_unweighted_knn_class_shapley(void *context_ptr){
     debug_print("%s", "Exact: Shapley Values done :)\n");
 }
 
-// Avoid context pointer chasing, use local variables
-void opt1_compute_single_unweighted_knn_class_shapley(void *context_ptr){
-    context_t *context = (context_t*)context_ptr;
-    debug_print("%s", "\nStart Shapley computation:\n");
-    
-    size_t size_x_trn = context->size_x_trn;
-    int* x_test_knn_gt = context->x_test_knn_gt;
-    double* y_trn = context->y_trn;
-    double* y_tst = context->y_tst;
-    double* sp_gt = context->sp_gt;
-    double K = context->K;
 
-    for(int j=0; j<context->size_x_tst;j++) {
-        // debug_print("  iteration: j=%d\n", j);
-        // Line 3 of Algo 1
-        int offset = x_test_knn_gt[j*size_x_trn+size_x_trn-1];
-        double tmp = (y_trn[offset] == y_tst[j]) ? 1.0 : 0.0;
-        sp_gt[j*size_x_trn + offset] = tmp / size_x_trn; 
-
-        for (int i=size_x_trn-2; i>-1; i--) {
-            // debug_print("    i=%d\n", i);
-            int index_j_i = j*size_x_trn+i;
-
-            double s_j_alpha_i_plus_1 = sp_gt[j*size_x_trn + x_test_knn_gt[index_j_i+1]];
-            double difference = (double)(y_trn[x_test_knn_gt[index_j_i]] == y_tst[j]) - 
-                                        (double)(y_trn[x_test_knn_gt[index_j_i+1]] == y_tst[j]);
-            double min_K_i = K < i+1 ? K : i+1;
-
-            // debug_print("      s_j=%f\n", s_j_alpha_i_plus_1);
-            // debug_print("      diff=%f (%f,%f), (%f,%f)\n", difference, y_trn[x_tst_knn_gt[index_j_i]], y_tst[j], y_trn[x_tst_knn_gt[index_j_i+1]], y_tst[j]);
-            // debug_print("      min_=%f\n", min_K_i);
-
-            sp_gt[j*size_x_trn + x_test_knn_gt[index_j_i]] = s_j_alpha_i_plus_1 + (difference / K) * (min_K_i / (i+1));
-        }
-    }
-    debug_print("%s", "Exact: Shapley Values done :)\n");
-}
